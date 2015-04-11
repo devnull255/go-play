2209cafe71f87957b760f4b49e70a692a998d57a
Michael Mabin <mabinm@comast.net>
2015-04-11 13:05:55 -0500

    modified to use Readdir instead and provide '[]' to distinguish directories from non-directories

--- fileio/dirs.go
+++ fileio/dirs.go
@@ -18,8 +18,13 @@ func main() {
   default_dir := os.Getenv("HOME")
 
   var start_dir string
+  var name string
 
   if len(os.Args) > 1  {
+     if os.Args[1] == "-h" || os.Args[1] == "-help" {
+        fmt.Println("Usage: dirs.go [dirname]")
+        os.Exit(0)
+     }
      start_dir = os.Args[1]
   } else {
      start_dir = default_dir
@@ -30,10 +35,15 @@ func main() {
   
   defer dir.Close()
 
-  dirnames, err := dir.Readdirnames(0)
+  direntries, err := dir.Readdir(0)
   check(err)
 
-  for i := 0;i < len(dirnames); i++ {
-    fmt.Println(dirnames[i])
+  for i := 0;i < len(direntries); i++ {
+    if direntries[i].IsDir() {
+       name = fmt.Sprintf("[%s]",direntries[i].Name())
+    } else {
+       name = direntries[i].Name()
+    }
+    fmt.Println(name)
   }
 }  
